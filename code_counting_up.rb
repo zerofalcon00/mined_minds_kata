67345e271de35b37df61f3a32c852e986d2349b4
Josh <mcnett88@gmail.com>
2016-08-03 13:48:31 -0400

    finished code of 1-100 replacing with mined minds

--- code_counting_up.rb
+++ code_counting_up.rb
@@ -1,12 +1,14 @@
 number = 0
 
 100.times do
- number = number + 1
-    if number % 3 == 0
-     puts "mined"
- elsif number % 5 == 0
- 	puts "minds"
- else puts number
- end
-  
+	number = number + 1
+	if number % 3 == 0 && number % 5 == 0
+		puts "minedminds"
+	elsif number % 3 == 0
+    	puts "mined"
+ 	elsif number % 5 == 0
+ 		puts "minds"
+ 	else 
+ 		puts number
+ 	end
 end
