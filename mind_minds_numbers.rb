c381198380a73cc4ea84eba43a6c49a45b455141
Josh <mcnett88@gmail.com>
2016-08-08 14:51:05 -0400

    modified code but still works

--- mind_minds_numbers.rb
+++ mind_minds_numbers.rb
@@ -1,10 +1,12 @@
 
 def mined_minds (number)
     if number % 3 == 0 && number % 5 ==0
-    	"mined_minds"
+    	"minedminds"
     elsif number % 3 == 0
     	"mined"
     elsif number % 5 == 0
         "minds"
+    else
+    	number
     end
 end
--- test_mined_minds_numbers.rb
+++ test_mined_minds_numbers.rb
@@ -12,6 +12,18 @@ class Test_mined_minds < Minitest::Test
   end
 
   def test_3_returns_mined_minds
-  	assert_equal("mined_minds", mined_minds(15))
+  	assert_equal("minedminds", mined_minds(15))
+  end
+  
+  def test_98_returns_98
+  	assert_equal(98, mined_minds(98))
+  end
+
+  def test_30_returns_mined_minds
+  	assert_equal("minedminds", mined_minds(30))
+  end
+  
+  def test_10_returns_minds
+  	assert_equal("minds", mined_minds(10))
   end
 end
