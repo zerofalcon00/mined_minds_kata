1a0e0bd97c112e9e785970a7cd067f7176d720e3
Josh <mcnett88@gmail.com>
2016-08-08 01:07:08 -0400

    complete 3 runs and return 3 assertions

--- mind_minds_numbers.rb
+++ mind_minds_numbers.rb
@@ -1,6 +1,8 @@
 
 def mined_minds (number)
-    if number % 3 == 0
+    if number % 3 == 0 && number % 5 ==0
+    	"mined_minds"
+    elsif number % 3 == 0
     	"mined"
     elsif number % 5 == 0
         "minds"
--- test_mined_minds_numbers.rb
+++ test_mined_minds_numbers.rb
@@ -3,11 +3,15 @@ require_relative "mind_minds_numbers.rb"
 
 class Test_mined_minds < Minitest::Test
   
-  def test_3_returns_mined 
+  def test_1_returns_mined 
   	assert_equal("mined", mined_minds(3))
   end
 
-  def test_4_returns_mined
-  	assert_equal("minds", mined_minds(5))
+  def test_2_returns_minds
+    	assert_equal("minds", mined_minds(5))
+  end
+
+  def test_3_returns_mined_minds
+  	assert_equal("mined_minds", mined_minds(15))
   end
 end
