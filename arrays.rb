e6b9789719c03be3c17137b5e95739a8cd00b03e
Josh <mcnett88@gmail.com>
2016-08-10 14:29:00 -0400

    array prints mined on 3

--- arrays.rb
+++ arrays.rb
@@ -1,3 +1,17 @@
-def create_minedminds_array
-	Array.new(100)
+#def create_minedminds_array
+my_array = []
+number = 1
+	
+	(1..100).each do |number|
+
+		if number % 3 == 0 
+	         puts "mined"
+		else 
+			puts number
+
+	end
 end
+print my_array
+
+
+#create array with names of class (robbie), get to random pairs
--- test_arrays.rb
+++ test_arrays.rb
@@ -5,7 +5,12 @@ require_relative "arrays.rb"
 class Test_array < Minitest::Test
 	def test_1_returns_array
 		minedminds_array = create_minedminds_array
-		assert_equal(100, minedminds_array.length)
+		assert_equal(100, minedminds_array)
+	end
+
+	def test_first_item_is_1
+		minedminds_array = create_minedminds_array
+		assert_equal(1, minedminds_array)
 	end
 		
 end
