1c37c7fee2398805be679cb9908247615f08f660
Josh <mcnett88@gmail.com>
2016-08-10 21:45:41 -0400

    array now has 100 100's

--- arrays.rb
+++ arrays.rb
@@ -1,12 +1,11 @@
 def create_minedminds_array
  my_array = [*1..100]
  number = 1
+    
+      100.times do |number|
+    	number = number + 1
 
-
- 
-     
-
-		if number % 3 == 0 && number % 5 == 0
+    	if number % 3 == 0 && number % 5 == 0
 			"MinedMinds"
 		elsif number % 3 == 0 
 	        "Mined"
@@ -15,7 +14,7 @@ def create_minedminds_array
 	    else 
 			number
 		end
-   
+	end
 end
 create_minedminds_array
 #create array with names of class (robbie), get to random pairs
